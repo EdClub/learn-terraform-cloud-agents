caaa8cdd03112dd5178494f6646624ffbd3f8440
Ramtin Kiany <ramtin@edclub.com>
2024-07-10 17:49:07 -0400

    learning local env variables+t2

--- main.tf
+++ main.tf
@@ -3,10 +3,11 @@ provider "null" {}
 variable "keyA" {
   description = "The environment variable keyA"
   type        = string
+  default     = ""
 }
 
 locals {
-  keyA = var.keyA != "" ? var.keyA : (lookup(var.env, "keyA", ""))
+  keyA = var.keyA != "" ? var.keyA : (lookup(env, "keyA", ""))
 }
 
 resource "null_resource" "get_keyA" {
@@ -18,5 +19,3 @@ resource "null_resource" "get_keyA" {
 output "keyA_value" {
   value = local.keyA
 }
-
-
