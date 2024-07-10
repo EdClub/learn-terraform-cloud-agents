6ae5ff4f763cf71f798ea45b509d2e043e2b7c96
Ramtin Kiany <ramtin@edclub.com>
2024-07-10 17:47:29 -0400

    learning local env variables+t1

--- main.tf
+++ main.tf
@@ -1,20 +1,22 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 provider "null" {}
-
-resource "null_resource" "get_keyA" {
-  provisioner "local-exec" {
-    command = "echo ${var.keyA}"
-  }
-}
 
 variable "keyA" {
   description = "The environment variable keyA"
   type        = string
 }
 
-output "keyA_value" {
-  value = var.keyA
+locals {
+  keyA = var.keyA != "" ? var.keyA : (lookup(var.env, "keyA", ""))
 }
+
+resource "null_resource" "get_keyA" {
+  provisioner "local-exec" {
+    command = "echo ${local.keyA}"
+  }
+}
+
+output "keyA_value" {
+  value = local.keyA
+}
+
 
