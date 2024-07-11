d32e9cd5859063474d88c5739e968137c0e81502
Ramtin Kiany <ramtin@edclub.com>
2024-07-11 02:32:48 -0400

    learning local env variables+t10

--- main.tf
+++ main.tf
@@ -37,3 +37,26 @@ output "pm_api_token_secret_value" {
   sensitive = true
 }
 
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.name
+  name  = "nginx"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
